ebabc465a55006817fbc0193f2d83043e341a21d
yhd <1003717456@qq.com>
2022-08-15 00:41:53 +0800

    push commit

--- src/main/java/com/yhd/git/GitText.java
+++ src/main/java/com/yhd/git/GitText.java
@@ -4,5 +4,6 @@ public class GitText {
     public static void main(String[] args) {
         System.out.println("hello git2");
         System.out.println("hello git4");
+        System.out.println("push git4");
     }
 }
