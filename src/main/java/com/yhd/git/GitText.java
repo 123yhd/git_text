f25588a1374c254ae2c1792a2b1d04125d08f89c
yhd <1003717456@qq.com>
2022-08-15 00:30:22 +0800

    hot-fix commit

--- src/main/java/com/yhd/git/GitText.java
+++ src/main/java/com/yhd/git/GitText.java
@@ -3,5 +3,6 @@ package com.yhd.git;
 public class GitText {
     public static void main(String[] args) {
         System.out.println("hello git2");
+        System.out.println("hello git4");
     }
 }
