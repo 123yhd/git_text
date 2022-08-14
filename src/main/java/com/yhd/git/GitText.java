c30cc2b6abe30df4e32071959a1c81a0c14d87ef
yhd <1003717456@qq.com>
2022-08-15 00:26:16 +0800

    second commit

--- src/main/java/com/yhd/git/GitText.java
+++ src/main/java/com/yhd/git/GitText.java
@@ -2,6 +2,6 @@ package com.yhd.git;
 
 public class GitText {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("hello git2");
     }
 }
